426c438cbcbae58bba8ca8fe5fa102d1c9628890
Clayton Singh <claytonsingh@gmail.com>
2023-03-24 23:23:51 -0700

    Initial API

---
+++ formatters.go
@@ -0,0 +1,187 @@
+package main
+
+import (
+	"net/url"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/filemode"
+	"github.com/go-git/go-git/v5/plumbing/object"
+)
+
+type Context struct {
+	*gin.Context
+	repository Repo
+	namespace  string
+	repo       string
+}
+
+func (this *Context) BuildUri(parts ...string) string {
+	return BuildUri(this.Context, parts...)
+}
+
+func BuildUri(c *gin.Context, parts ...string) string {
+	u := url.URL{
+		Scheme: "http",
+		Host:   c.Request.Host,
+		Path:   strings.Join(parts, "/"),
+	}
+	if c.Request.Header.Get("X-Forwarded-Proto") == "https" {
+		u.Scheme = "https"
+	}
+	return u.String()
+}
+
+type Repo struct {
+	name        string
+	description string
+	url         string
+	repo        *git.Repository
+}
+
+func FormatRef(c *gin.Context, m plumbing.Hash, path string) gin.H {
+	hash := m.String()
+	return gin.H{
+		"sha": hash,
+		"url": BuildUri(c, "repos", c.Param("namespace"), c.Param("repo"), path, hash),
+	}
+}
+
+func FormatCommitRef(c *gin.Context, m plumbing.Hash) gin.H {
+	return FormatRef(c, m, "git/commits")
+}
+
+func FormatCommitFull(c *gin.Context, m *object.Commit) gin.H {
+	hash := m.Hash.String()
+	p := make([]any, 0)
+	m.Parents().ForEach(func(m *object.Commit) error {
+		p = append(p, FormatCommitRef(c, m.Hash))
+		return nil
+	})
+	return gin.H{
+		"sha":       hash,
+		"url":       BuildUri(c, "repos", c.Param("namespace"), c.Param("repo"), "git/commits", hash),
+		"message":   m.Message,
+		"author":    FormatSignature(c, m.Author),
+		"committer": FormatSignature(c, m.Committer),
+		"parents":   p,
+		"tree":      FormatTreeRef(c, m.TreeHash),
+	}
+}
+
+func FormatFile(c *gin.Context, f *object.File, m plumbing.Hash) gin.H {
+	return gin.H{
+		"url": BuildUri(c, "repos", c.Param("namespace"), c.Param("repo"), "git/blobs", f.Hash.String()),
+		//"raw_url": BuildUri(c, "repos", c.Param("namespace"), c.Param("repo"), "git/blobs", f.Hash.String(), "raw"),
+		"raw_url": BuildUri(c, "repos", c.Param("namespace"), c.Param("repo"), "raw", m.String(), f.Name),
+		"sha":     f.Hash.String(),
+		"mode":    f.Mode.String(),
+		"path":    f.Name,
+		"type":    "blob",
+		"size":    f.Size,
+	}
+}
+
+func FormatSignature(c *gin.Context, a object.Signature) gin.H {
+	return gin.H{
+		"date":  a.When.UTC(),
+		"name":  a.Name,
+		"email": a.Email,
+	}
+}
+
+func FormatTreeRef(c *gin.Context, m plumbing.Hash) gin.H {
+	return FormatRef(c, m, "git/trees")
+}
+
+func FormatBranchRef(c Context, branch *plumbing.Reference) gin.H {
+	name := branch.Name().Short()
+	return gin.H{
+		"name":   name,
+		"url":    c.BuildUri("repos", c.namespace, c.repo, "branches", name),
+		"commit": FormatCommitRef(c.Context, branch.Hash()),
+	}
+}
+
+func FormatBranchFull(c Context, branch *plumbing.Reference, commit *object.Commit) gin.H {
+	name := branch.Name().Short()
+	return gin.H{
+		"name":   name,
+		"url":    c.BuildUri("repos", c.namespace, c.repo, "branches", name),
+		"commit": FormatCommitFull(c.Context, commit),
+	}
+}
+
+func FormatTagRef(c Context, ref *plumbing.Reference, tag *object.Tag) gin.H {
+	if tag == nil {
+		name := ref.Name().Short()
+		return gin.H{
+			"sha":     nil,
+			"tag":     name,
+			"url":     c.BuildUri("repos", c.namespace, c.repo, "tags", name),
+			"commit":  FormatCommitRef(c.Context, ref.Hash()),
+			"message": "",
+			"tagger":  nil,
+		}
+	} else {
+		name := ref.Name().Short()
+		return gin.H{
+			"sha":     ref.Hash().String(),
+			"tag":     name,
+			"url":     c.BuildUri("repos", c.namespace, c.repo, "tags", name),
+			"commit":  FormatCommitRef(c.Context, tag.Target),
+			"message": tag.Message,
+			"tagger":  FormatSignature(c.Context, tag.Tagger),
+		}
+	}
+}
+
+func FormatTagFull(c Context, tag *plumbing.Reference, commit *object.Commit) gin.H {
+	name := tag.Name().Short()
+	return gin.H{
+		"tag":    name,
+		"url":    c.BuildUri("repos", c.namespace, c.repo, "tags", name),
+		"commit": FormatCommitFull(c.Context, commit),
+	}
+}
+
+func FormatTree(c Context, tree *object.Tree, recursive bool) gin.H {
+	r := make([]any, 0)
+	if recursive {
+		tree.Files().ForEach(func(f *object.File) error {
+			r = append(r, FormatFile(c.Context, f, tree.Hash))
+			return nil
+		})
+	} else {
+		for _, entry := range tree.Entries {
+			if entry.Mode.IsFile() {
+				blob, _ := object.GetBlob(c.repository.repo.Storer, entry.Hash)
+				file := object.NewFile(entry.Name, entry.Mode, blob)
+				r = append(r, FormatFile(c.Context, file, tree.Hash))
+			} else if entry.Mode == filemode.Dir {
+				r = append(r, gin.H{
+					"path": entry.Name,
+					"mode": entry.Mode,
+					"type": "tree",
+					"sha":  entry.Hash.String(),
+					"url":  c.BuildUri("repos", c.namespace, c.repo, "git/trees", entry.Hash.String()),
+				})
+			} else if entry.Mode == filemode.Submodule {
+				r = append(r, gin.H{
+					"path": entry.Name,
+					"mode": entry.Mode,
+					"type": "submodule",
+					"sha":  entry.Hash.String(),
+					// "url":  c.BuildUri("repos", c.namespace, c.repo, "git/trees", entry.Hash.String()),
+				})
+			}
+		}
+	}
+	return gin.H{
+		"url":  c.BuildUri("repos", c.namespace, c.repo, "git/trees", tree.Hash.String()),
+		"sha":  tree.Hash.String(),
+		"tree": r,
+	}
+}
---
+++ main.go
@@ -0,0 +1,238 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+)
+
+func Panic1[T any](a T, e error) T {
+	if e != nil {
+		panic(e)
+	}
+	return a
+}
+
+func main() {
+	repos := make(map[string]Repo)
+	repos["local/self"] = Repo{
+		name:        "test",
+		description: "",
+		url:         "local/self",
+		repo:        Panic1(git.PlainOpen("./")),
+	}
+
+	WithRepo := func(handler func(Context)) func(*gin.Context) {
+		return func(c *gin.Context) {
+			namespace := c.Param("namespace")
+			reponame := c.Param("repo")
+			repository, ok := repos[fmt.Sprintf("%s/%s", namespace, reponame)]
+			if ok {
+				handler(Context{
+					Context:    c,
+					repository: repository,
+					namespace:  namespace,
+					repo:       reponame,
+				})
+				return
+			}
+			c.JSON(http.StatusNotFound, gin.H{"error": "not found"})
+		}
+	}
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusTemporaryRedirect, "/repos")
+	})
+
+	r.GET("/repos", func(c *gin.Context) {
+		r := make([]any, 0)
+		for _, repo := range repos {
+			r = append(r, gin.H{
+				"name":        repo.name,
+				"description": repo.description,
+				"url":         BuildUri(c, "repos", repo.url),
+			})
+		}
+		c.IndentedJSON(http.StatusOK, r)
+	})
+
+	// Repo index
+	r.GET("/repos/:namespace/:repo/", WithRepo(func(c Context) {
+		c.IndentedJSON(http.StatusOK, gin.H{
+			"name":        c.repository.name,
+			"description": c.repository.description,
+			"url":         c.BuildUri("repos", c.repository.name),
+			"indexes": gin.H{
+				"branches": c.BuildUri("repos", c.namespace, c.repo, "branches"),
+				"tags":     c.BuildUri("repos", c.namespace, c.repo, "tags"),
+				// "commits":  c.BuildUri("repos", c.namespace, c.repo, "git/commits"),
+			},
+		})
+	}))
+
+	// branch index
+	r.GET("/repos/:namespace/:repo/branches", WithRepo(func(c Context) {
+		r := make([]any, 0)
+		if branches, err := c.repository.repo.Branches(); err == nil {
+			branches.ForEach(func(t *plumbing.Reference) error {
+				r = append(r, FormatBranchRef(c, t))
+				return nil
+			})
+			c.IndentedJSON(http.StatusOK, r)
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+	}))
+
+	r.GET("/repos/:namespace/:repo/branches/:branch", WithRepo(func(c Context) {
+		if branch, err := c.repository.repo.Storer.Reference(plumbing.NewBranchReferenceName(c.Param("branch"))); err == nil {
+			if commit, err := c.repository.repo.CommitObject(branch.Hash()); err == nil {
+				c.IndentedJSON(http.StatusOK, FormatBranchFull(c, branch, commit))
+				return
+			}
+		}
+		c.JSON(http.StatusNotFound, gin.H{"error": "not found"})
+	}))
+
+	// tag index
+	r.GET("/repos/:namespace/:repo/tags", WithRepo(func(c Context) {
+		r := make([]any, 0)
+		if tags, err := c.repository.repo.Tags(); err == nil {
+			tags.ForEach(func(t *plumbing.Reference) error {
+				if ref, err := c.repository.repo.Tag(c.Param("tag")); err == nil {
+					tag, err := c.repository.repo.TagObject(ref.Hash())
+					switch err {
+					case nil:
+						if tag.TargetType == plumbing.CommitObject {
+							r = append(r, FormatTagRef(c, ref, tag))
+						}
+					case plumbing.ErrObjectNotFound:
+						if _, err := c.repository.repo.CommitObject(ref.Hash()); err == nil {
+							r = append(r, FormatTagRef(c, ref, nil))
+						}
+					}
+				}
+				return nil
+			})
+			c.IndentedJSON(http.StatusOK, r)
+			return
+		}
+		c.JSON(http.StatusNotFound, gin.H{"error": "not found"})
+	}))
+
+	r.GET("/repos/:namespace/:repo/tags/:tag", WithRepo(func(c Context) {
+		if ref, err := c.repository.repo.Tag(c.Param("tag")); err == nil {
+			tag, err := c.repository.repo.TagObject(ref.Hash())
+			switch err {
+			case nil:
+				if tag.TargetType == plumbing.CommitObject {
+					c.IndentedJSON(http.StatusOK, FormatTagRef(c, ref, tag))
+					return
+				}
+			case plumbing.ErrObjectNotFound:
+				if _, err := c.repository.repo.CommitObject(ref.Hash()); err == nil {
+					c.IndentedJSON(http.StatusOK, FormatTagRef(c, ref, nil))
+					return
+				}
+			default:
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+		}
+		c.JSON(http.StatusNotFound, gin.H{"error": "not found"})
+	}))
+
+	// commit index
+	r.GET("/repos/:namespace/:repo/git/commits", WithRepo(func(c Context) {
+		r := make([]any, 0)
+		if commit, err := c.repository.repo.CommitObjects(); err == nil {
+			commit.ForEach(func(m *object.Commit) error {
+				cmt := FormatCommitRef(c.Context, m.Hash)
+				cmt["message"] = m.Message
+				cmt["author"] = FormatSignature(c.Context, m.Author)
+				cmt["committer"] = FormatSignature(c.Context, m.Committer)
+				r = append(r, cmt)
+				return nil
+			})
+			c.IndentedJSON(http.StatusOK, r)
+			return
+		}
+		c.JSON(http.StatusNotFound, gin.H{"error": "not found"})
+	}))
+
+	r.GET("/repos/:namespace/:repo/git/commits/:sha", WithRepo(func(c Context) {
+		sha := plumbing.NewHash(c.Param("sha"))
+		if commit, err := c.repository.repo.CommitObject(sha); err == nil {
+			c.IndentedJSON(http.StatusOK, FormatCommitFull(c.Context, commit))
+			return
+		}
+		c.JSON(http.StatusNotFound, gin.H{"error": "not found"})
+	}))
+
+	// trees
+	r.GET("/repos/:namespace/:repo/git/trees/:sha", WithRepo(func(c Context) {
+		var recursive = false
+		switch strings.ToLower(c.Query("recursive")) {
+		case "1", "true", "yes":
+			recursive = false
+		}
+		sha := plumbing.NewHash(c.Param("sha"))
+		if tree, err := c.repository.repo.TreeObject(sha); err == nil {
+			// tree.Entries
+			c.IndentedJSON(http.StatusOK, FormatTree(c, tree, recursive))
+			return
+		}
+		c.JSON(http.StatusNotFound, gin.H{"error": "not found"})
+	}))
+
+	// blobs
+	r.GET("/repos/:namespace/:repo/git/blobs/:sha", WithRepo(func(c Context) {
+		sha := plumbing.NewHash(c.Param("sha"))
+		if blob, err := c.repository.repo.BlobObject(sha); err == nil {
+			if r, err := blob.Reader(); err == nil {
+				defer r.Close()
+
+				buff := new(bytes.Buffer)
+				buff.ReadFrom(r)
+
+				c.IndentedJSON(http.StatusOK, gin.H{
+					"url":     c.BuildUri("repos", c.namespace, c.repo, "git/blobs", sha.String()),
+					"sha":     sha.String(),
+					"size":    blob.Size,
+					"content": base64.RawStdEncoding.EncodeToString(buff.Bytes()),
+				})
+				return
+			}
+		}
+		c.JSON(http.StatusNotFound, gin.H{"error": "not found"})
+	}))
+
+	// raw file download
+	r.GET("/repos/:namespace/:repo/raw/:sha/*path", WithRepo(func(c Context) {
+		sha := plumbing.NewHash(c.Param("sha"))
+		if tree, err := c.repository.repo.TreeObject(sha); err == nil {
+			if tree, err := tree.File(c.Param("path")[1:]); err == nil {
+				blob := tree.Blob
+				if reader, err := blob.Reader(); err == nil {
+					defer reader.Close()
+
+					c.DataFromReader(http.StatusOK, blob.Size, "application/octet-stream", reader, nil)
+					return
+				}
+			}
+		}
+		c.JSON(http.StatusNotFound, gin.H{"error": "not found"})
+	}))
+
+	log.Fatal(r.Run()) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
